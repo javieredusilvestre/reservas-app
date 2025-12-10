4b313c81e22dd2a0a94c54fbffb1e92c60cd04fd
javieredusilestre <javieredusilestre@gmail.com>
2025-12-10 12:18:52 -0300

    Cambio de size titulo

--- src/App.js
+++ src/App.js
@@ -372,7 +372,7 @@ function AppContent() {
       
       {/* HEADER con diseño hotelero */}
       <header className="navbar navbar-expand-lg navbar-light bg-light rounded shadow-sm px-4 mb-4">
-        <h1 className="navbar-brand h3 mb-0">Sistema de Reserva de Cabañas - Parque Futangue</h1>
+        <h1 className="navbar-brand h3 mb-0">Reserva de Cabañas - Parque Futangue</h1>
         
         <nav className="main-nav d-flex flex-grow-1 justify-content-end align-items-center">
           
